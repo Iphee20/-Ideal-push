80831b10fd31e50c18c9954e072b646f7faa2d91
Glora Egedi Nkwocha <ifeomaegedi-hash>
2025-08-26 15:22:47 +0100

    third commit

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-let number = 38 
-console.log(number)
+let side1 = 50
+let side2 = 100
+let side3 = 70
 
-if (number % 2 === 0) {
-console.log(number + "is even"); 
-} else {
-console.log (number + "ïs odd");
+console.log(side3)
+
+if (side1 === side2 && side2 === side3) {
+console.log("Equilateral triangle");
+
+} else if (side1 === side2); (side1 === side3); (side2 === side3); {
+console.log("Isosceles Triangle");}
+
+if (side1 !== side2); (side1 !== side3); (side2 !== side3);{
+    console.log("scalene triangle");
 }
-
