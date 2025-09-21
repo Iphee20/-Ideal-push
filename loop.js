2290f00ed51a6286946f03c14bae630372d96090
Glora Egedi Nkwocha <ifeomaegedi-hash>
2025-09-21 20:06:04 +0100

    forth commit

---
+++ loop.js
@@ -0,0 +1,9 @@
+let toy = 1 //starting point
+while(toy <= 20){  // condition to stop)
+    if(toy===1){
+        console.log(toy + " toy")
+    } else {
+        console.log(toy + " toys")
+    }
+        toy = toy + 2 //how to iterate
+    }
